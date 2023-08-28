5126104f1a2e8a225cb2b091ebc628f5bce55327
LKTOKB <theodiamanttt@gmail.com>
2023-08-28 20:29:01 +0300

    Start to create the routes and the link between the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
+import { Routes, Route, Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
+import LeftBar from "./components/LeftBar/LeftBar";
+import RightBar from "./components/RightBar/RightBar";
 
 function App() {
+
+  const Layout = () => {
+    return(
+      <div>
+        <Navbar />
+        <div style={{display:'flex'}}>
+        <LeftBar />
+        <Outlet />
+        <RightBar />
+        </div>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element: <Home />
+        },
+        {
+          path:"/profile/:id",
+          element: <Profile />
+        }
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ])
+
   return(
-      <Login />
+    <div>
+      <RouterProvider router={router} />
+    </div>
     )
 }
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,20 @@
-import "./Login.scss"
-
+import "./Login.scss";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="login">
       <div className="card">
         <div className="left">
-          <h1>Hello World</h1>
-          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
             alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.</p>
-            <span>Don't you have an account?</span>
-        <button>Register</button>
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
         </div>
         <div className="right">
           <h1>Login</h1>
@@ -22,7 +26,7 @@ const Login = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
