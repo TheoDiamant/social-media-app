f47a58a38e82d465e7efc2c145e8135a4b4b7d57
LKTOKB <theodiamanttt@gmail.com>
2023-08-31 18:08:51 +0300

    Register routes create with bcrypt to protect the password, route for login create, which generate a unique token, and logout, all work

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
-import express from "express"
+import express from "express";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express()
+const app = express();
+
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
 
 app.listen(8800, () => {
-    console.log("Connected to backend")
-})
+  console.log("Connected to backend");
+});
