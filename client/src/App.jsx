e85957cba720b47a3408fdde394b326cf15f1efe
LKTOKB <theodiamanttt@gmail.com>
2023-09-02 13:06:25 +0300

    Use react tan query to make query and use diferent ways to fetch data from the api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,27 +13,36 @@ import Profile from "./pages/Profile/Profile";
 import Navbar from "./components/Navbar/Navbar";
 import LeftBar from "./components/LeftBar/LeftBar";
 import RightBar from "./components/RightBar/RightBar";
-import "./style.scss"
+import "./style.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
 function App() {
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
   const { darkMode } = useContext(DarkModeContext);
 
+  const queryClient = new QueryClient()
+
   const Layout = () => {
     return (
-      <div className={`theme-${darkMode ? "dark" : "light"}`}>
-        <Navbar />
-        <div style={{ display: "flex" }}>
-          <LeftBar />
-          <div style={{ flex: 6 }}>
-            <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <div className={`theme-${darkMode ? "dark" : "light"}`}>
+          <Navbar />
+          <div style={{ display: "flex" }}>
+            <LeftBar />
+            <div style={{ flex: 6 }}>
+              <Outlet />
+            </div>
+            <RightBar />
           </div>
-          <RightBar />
         </div>
-      </div>
+      </QueryClientProvider>
     );
   };
 
@@ -50,7 +59,7 @@ function App() {
       path: "/",
       element: (
         // <ProtectedRoute>
-          <Layout />
+        <Layout />
         // </ProtectedRoute>
       ),
       children: [
