2a2556509d64b58711c4c691e2c0c055d228c6e6
LKTOKB <theodiamanttt@gmail.com>
2023-09-06 01:40:39 +0300

    Like functionality implemented

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -4,8 +4,6 @@ import jwt from "jsonwebtoken";
 import moment from "moment";
 
 export const getComments = (req, res, next) => {
-
-
   // Use placeholders for user IDs
   const q = `SELECT c.*, u.id AS userId, name, profilePic 
       FROM comments AS c 
@@ -19,7 +17,6 @@ export const getComments = (req, res, next) => {
   });
 };
 
-
 export const addComment = (req, res) => {
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).json("Not logged in!");
@@ -27,14 +24,13 @@ export const addComment = (req, res) => {
   jwt.verify(token, "secretkey", (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid!");
 
-   
-
-    const q = "INSERT INTO comments(`desc`, `createdAt`, `userId`, `postId`) VALUES (?)";
+    const q =
+      "INSERT INTO comments(`desc`, `createdAt`, `userId`, `postId`) VALUES (?)";
     const values = [
       req.body.desc,
       moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
       userInfo.id,
-      req.body.postId
+      req.body.postId,
     ];
 
     db.query(q, [values], (err, data) => {
@@ -43,3 +39,4 @@ export const addComment = (req, res) => {
     });
   });
 };
+
--- client/src/components/Comments/Comments.jsx
+++ client/src/components/Comments/Comments.jsx
@@ -52,7 +52,7 @@ const Comments = ({ postId }) => {
         ? "Loading..."
         : data.map((comment) => (
             <div className="comment">
-              <img src={comment.profilePicture} alt="" />
+              <img src={comment.profilePic} alt="" />
               <div className="info">
                 <span>{comment.name}</span>
                 <p>{comment.desc}</p>
