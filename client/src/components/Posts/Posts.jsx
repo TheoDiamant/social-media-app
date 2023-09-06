fbcf07df77e21ffa36f03af3b28543b9db235dcc
LKTOKB <theodiamanttt@gmail.com>
2023-09-06 18:01:08 +0300

    V1 of the project finish, a lot of functionality to implement still

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -4,9 +4,10 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { makeRequest } from "../../axios";
 
-const Posts = () => {
+const Posts = ({userId}) => {
+
   const { isLoading, error, data } = useQuery(["posts"], () =>
-    makeRequest.get("/posts").then((res) => {
+    makeRequest.get("/posts?userId=" +userId).then((res) => {
       return res.data;
     })
   );
